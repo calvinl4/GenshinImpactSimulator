a4f84c3c888ac4687bbef824e760eebed73c00b4
calvinl4 <calvinlin@live.ca>
2020-11-01 17:25:44 -0500

    finished weaponCard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
+import WeaponCard from "./components/weaponCard/weaponCard";
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div style={{height: "30px"}}></div>
+       <div style={{display: "flex"}}><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard></div>
+
+      <div style={{height: "30px"}}></div>
+      <div style={{display: "flex"}}><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard><WeaponCard weaponImage="4_lions_roar.png"></WeaponCard></div>
     </div>
   );
 }
--- frontend/src/components/weaponCard/weaponCard.jsx
+++ frontend/src/components/weaponCard/weaponCard.jsx
@@ -1,12 +1,29 @@
-const weaponCard = (props) => {
+import React from "react";
+import "./weaponCard.scss"
+import exampleImage from "../../assets/weapons/sword/4Star/4_lions_roar.png"
+
+export const WeaponCard = (props) => {
+  var imagePath = "../../assets/weapons/sword/4Star/" + `${props.weaponImage}`;
+  // require(`${imagePath}`)
   return (
     <div>
-      <div className="">
+      <div className={"cardContainer"}>
+        <h1 className={"titleText"}>4 Star Weapon</h1>
+        <img className={"weaponImage"} src={exampleImage} alt="weaponImage"></img>
+        <p className={"weaponStats"}>
+          Bane of Fire and Thunder: - Increases DMG against enemies affected by Pyro or Electro by 20/25/30/35/40%.
+          {/* {props.weaponStats} */}
+        </p>
 
+
+         <p className={"weaponDesc"}>
+           {/* {props.weaponDesc} */}
+          A sharp blade with extravagant carvings that somehow does not compromise on durability and shapness. It roars like a lion as it cuts through the air.
+        </p>
       </div>
 
     </div>
   )
 }
 
-export default weaponCard;
+export default WeaponCard
