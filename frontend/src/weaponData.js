7f6c8bdae8837d299465343ecea43a2518189aa9
calvinl4 <calvinlin@live.ca>
2020-11-11 21:39:32 -0500

    Update weaponData.js

--- frontend/src/weaponData.js
+++ frontend/src/weaponData.js
@@ -22,10 +22,64 @@ export const weaponData = [
   { // 5 star weapons
     weapons: [
       {
-
+        weaponTitle: "5 Star Bow",
+        weaponImage: "/weapons/bow/5Star/5_amos_bow.png",
+        weaponAbility: "Increases Normal Attack and Aimed Shot DMG by 12/15/18/21/24%. Increases DMG by 8/10/12/14/16% for every 0.1s that an arrow is in flight, up to 0.5s.",
+        weaponDesc: "An extremely ancient bow that has retained its power despite its original master being long gone. It draws power from everyone and everything in the world, and the further away you are from that which your heart desires, the more powerful it is.",
       },
       {
-
+        weaponTitle: "5 Star Bow",
+        weaponImage: "/weapons/bow/5Star/5_skyward_harp.png",
+        weaponAbility: "Increases CRIT DMG by 20/25/30/35/40%. Hits have a 60/70/80/90/100% chance to inflict a small AoE attack, dealing 100% Physical ATK DMG. Can only occur once every 4/3.5/3/2.5/4s.",
+        weaponDesc: "A greatbow that symbolizes Dvalins's affiliation with the Anemo Archon. The sound of the bow firing is music to the Anemo Archon's ears. It contains the power of sky and wind within.",
+      },
+      {
+        weaponTitle: "5 Star Catalyst",
+        weaponImage: "/weapons/catalyst/5Star/5_lost_prayer.png",
+        weaponAbility: "Increases Movement Speed SPD by 10%. When in battle, earn a 6/8/10/12/14% Elemental DMG Bonus every 4s. Max 4 stacks. Lasts until the character falls or leaves combat.",
+        weaponDesc: "An educational tome written by anonymous early inhabitants who worshiped the wind. It has been blessed by the wind for its faithfulness and influence over the millennia.",
+      },
+      {
+        weaponTitle: "5 Star Catalyst",
+        weaponImage: "/weapons/catalyst/5Star/5_skyward_atlas.png",
+        weaponAbility: "Wandering Clouds: -Increases Elemental DMG Bonus by 12/15/18/21/24%, normal Attack hits have a 50% chance to earn the favor of the clouds, which actively seek out nearby enemies to attack for 15s.",
+        weaponDesc: "A cloud atlas symbolizing Dvalin and its former master, the Anemo Archon. It details the winds and clouds of the northern regions and contains the powers of the sky and wind.",
+      },
+      {
+        weaponTitle: "5 Star Claymore",
+        weaponImage: "/weapons/claymore/5Star/5_skyward_pride.png",
+        weaponAbility: "Sky-ripping Dragon Spine: -Increases all DMG by 8/10/12/14/16%. After using an Elemental Burst, Normal or Charged Attack, on hit, creates a vacuum blade.",
+        weaponDesc: "A claymore that symbolizes the pride of Dvalin soaring through the skies. When swung, it emits a deep hum as the full force of Dvalin's command of the sky and the wind is unleashed.",
+      },
+      {
+        weaponTitle: "5 Star Claymore",
+        weaponImage: "/weapons/claymore/5Star/5_wolfs_gravestone.png",
+        weaponAbility: "Wolfish Tracker: -Increases ATK by 20/25/30/35/40%. On hit, attacks against enemies with less than 30% HP increase all party members' ATK",
+        weaponDesc: "A longsword used by the Wolf Knight. Originally just a heavy sheet of iron given to the knight by a blacksmith from the city, it became endowed with legendary power owing to his friendship with the wolves.",
+      },
+      {
+        weaponTitle: "5 Star Polearm",
+        weaponImage: "/weapons/polearm/5Star/5_primordial_jade.png",
+        weaponStats: "Eagle Spear of Justice: -On hit, increases ATK by 3.2/3.9/4.6/5.3/6% for 6s, Max 7 stacks. Can only occur once every 0.3s.",
+        weaponDesc: "A jade polearm made by the archons, capable of slaying ancient beasts",
+      },
+      {
+        weaponTitle: "5 Star Polearm",
+        weaponImage: "/weapons/polearm/5Star/5_skyward_spine.png",
+        weaponStats: "Blackwing: -Increases CRIT Rate by 8/10/12/14/16% and increases Normal ATK SPD by 12%. Additionally, Normal and Charged Attacks hits on enemies have a 50% chance to trigger a vacuum blade",
+        weaponDesc: "A polearm that symbolizes Dvalin's firm resolve. The upright shaft of this weapon points towards the heavens, clad in the might of sky and wind.",
+      },
+      {
+        weaponTitle: "5 Star Sword",
+        weaponImage: "/weapons/sword/5Star/5_aquila_favonia.png",
+        weaponStats: "Falcon's Defiance: -ATK is increased by 20/25/30/35/40%. Triggers on taking DMG: the soul of the Falcon of the West awakens, holding the banner of resistance aloft",
+        weaponDesc: "The soul of the Knights of Favonius. Millennia later, it still calls on the winds of swift justice to vanquish all evil - just like the last heroine who wielded it",
+      },
+      {
+        weaponTitle: "5 Star Sword",
+        weaponImage: "/weapons/sword/5Star/5_skyward_blade.png",
+        weaponStats: "Sky-Piercing Fang: -CRIT Rate increased by 4/5/6/7/8%. Gains Skypiercing Might upon using an Elemental Burst: Increases Movement SPD by 10%",
+        weaponDesc: "A sword of a knight that symbolizes the reclaimed honor of the Stormterror. The blessings of the God of Winds rest on the fuller of the blade, imbuing the sword with the powers of the sky and the wind",
       },
     ]
   }
